c77374f63d6cc5bf5f4a08cf140bf9fa83b55200
msdeepak052 <160315954+msdeepak052@users.noreply.github.com>
2025-06-02 00:39:57 +0530

    Update jenkins.sh

--- userdata/jenkins.sh
+++ userdata/jenkins.sh
@@ -3,34 +3,6 @@
 # Exit immediately if any command fails
 set -e
 
-# set -euxo pipefail   #The set -euxo pipefail in scripts helps catch such issues early
-# # Install Jenkins
-# sudo yum update -y
-# sudo yum install wget unzip -y
-
-# # Install Java 21 (for RHEL 10)
-# sudo dnf install -y java-21-openjdk-devel
-
-# # Configure alternatives if needed
-# sudo alternatives --set java /usr/lib/jvm/java-21-openjdk-*/bin/java
-# sudo alternatives --set javac /usr/lib/jvm/java-21-openjdk-*/bin/javac
-
-# sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-# # sudo yum install jenkins java-11-openjdk-devel -y
-# sudo systemctl daemon-reload
-# sudo systemctl start jenkins
-# sudo systemctl enable jenkins
-
-# # Install Docker
-# sudo yum install docker.io -y
-# sudo systemctl start docker
-# sudo systemctl enable docker
-# sudo usermod -aG docker jenkins
-# sudo usermod -aG docker ec2-user
-
-# # Install other tools
-# sudo yum install -y git maven
 
 sudo apt update -y
 sudo apt install wget -y
